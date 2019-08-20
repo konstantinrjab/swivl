6db4837625036600181a213bc2e589b7fce6729d
konstantinrjab <konstantinrjab@gmail.com>
2019-08-20 20:00:15 +0300

    installed doctrine orm

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,7 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
